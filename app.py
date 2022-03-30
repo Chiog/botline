b6754e75af83196e72713b599b6a5c08bed7e04c
Chiog <brucelin150131@gmail.com>
2022-03-31 06:14:14 +0800

    string

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token, #要給token才能執行
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text='早安'))
 
 
 if __name__ == "__main__":
